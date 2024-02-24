d111b8d4a40cbf49e19492497719899e9944c920
Adriaan Mouton <arouxmouton@gmail.com>
2024-02-24 20:11:33 +0200

    RDH-20: gallery

--- resources/views/components/block/standard-header.blade.php
+++ resources/views/components/block/standard-header.blade.php
@@ -1,4 +1,4 @@
-<div class="flex w-full items-center justify-center bg-cover bg-center bg-no-repeat py-24"
+<div class="flex w-full items-center justify-center bg-cover bg-center bg-no-repeat py-24 md:py-48"
     style="background-image: {{ $background ?? '' }}">
     <h1 class="font-viaoda text-offwhite text-5xl md:text-7xl">
         {{ $title ?? '' }}
