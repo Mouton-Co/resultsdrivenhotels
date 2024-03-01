4233c461607520a3105e29df02ed3256ef0f29f7
Adriaan Mouton <arouxmouton@gmail.com>
2024-03-01 14:14:50 +0200

    RDH-24: added accommodation carousel content

---
+++ config/accommodation-and-restourants.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        'title' => 'Hotel Krige',
+        'description' => "Inaugurated in September 2021, this graded four-star hotel, nestled in central Stellenbosch, epitomizes the town's charm and vibrant university town culture.",
+        'image' => 'images/general/hotel-krige.png',
+        'link' => 'https://hotelkrige.co.za/',
+    ],
+    [
+        'title' => '38onWestcliff',
+        'description' => "38onWestcliff presents a lavish six-bedroom oceanfront residence in the serene Westcliff area of Hermanus, accommodating up to twelve guests in ultimate luxury and comfort.",
+        'image' => 'images/general/38onWestcliff.png',
+        'link' => 'https://www.38onwestcliff.co.za/',
+    ],
+    [
+        'title' => 'Manhattan On Coral',
+        'description' => "A touch of New York on Blouberg beachfront!",
+        'image' => 'images/general/ManhattanOnCoral.png',
+        'link' => 'https://www.themanhattanoncoral.co.za/',
+    ],
+    [
+        'title' => 'The Franschhoek Beer Company',
+        'description' => "Nestled in the heart of what we believe to be the most beautiful place on earth. We are so grateful we get to call Franschhoek “home”. We love it here. We love our beer, and we wouldn't want to brew it anywhere else.",
+        'image' => 'images/general/beerco.png',
+        'link' => 'https://franschhoekbeerco.co.za/',
+    ],
+    [
+        'title' => 'Franks Corner',
+        'description' => "Frank's Corner Bar and Grill emerges as an exciting new casual dining spot, quickly becoming a local favourite, under the guidance of our esteemed celebrity chef Rueben Riffel alongside chef Aviv Liebenberg.",
+        'image' => 'images/general/FransCorner.png',
+        'link' => 'https://franks-corner.com/',
+    ],
+];
