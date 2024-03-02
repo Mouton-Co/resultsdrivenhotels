241631f0ec461eed65a967fd517f36d473cdd0fd
Adriaan Mouton <arouxmouton@gmail.com>
2024-03-02 19:43:01 +0200

    hotfix: remove ssl enforcing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::component('app-layout', AppLayout::class);
-        \URL::forceScheme('https');
     }
 }
